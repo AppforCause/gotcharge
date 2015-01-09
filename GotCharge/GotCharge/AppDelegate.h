551ff070862423ffb4df275dc88313d5c939c524
Hemen Chhatbar <hemenc@slalom.com>
2015-01-09 14:17:44 -0800

    project set up

---
+++ GotCharge/GotCharge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GotCharge
+//
+//  Created by Hemen Chhatbar on 1/9/15.
+//  Copyright (c) 2015 appforcause. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
