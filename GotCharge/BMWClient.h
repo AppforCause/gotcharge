2c2809b405a0f64daedfb2e7d7cbc3c4ebd90b1a
Hunaid <hunaid@hotmail.com>
2015-01-09 14:45:11 -0800

    Added empty BMW client

---
+++ GotCharge/BMWClient.h
@@ -0,0 +1,13 @@
+//
+//  BMWClient.h
+//  GotCharge
+//
+//  Created by Hunaid Hussain on 1/9/15.
+//  Copyright (c) 2015 appforcause. All rights reserved.
+//
+
+#import "BDBOAuth1RequestOperationManager.h"
+
+@interface BMWClient : BDBOAuth1RequestOperationManager
+
+@end
---
+++ GotCharge/BMWClient.m
@@ -0,0 +1,13 @@
+//
+//  BMWClient.m
+//  GotCharge
+//
+//  Created by Hunaid Hussain on 1/9/15.
+//  Copyright (c) 2015 appforcause. All rights reserved.
+//
+
+#import "BMWClient.h"
+
+@implementation BMWClient
+
+@end
