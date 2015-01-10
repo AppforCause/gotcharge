00a11c5a7e8533fc2588b9da9b27f6fa566cbe44
Hemen Chhatbar <hemenc@slalom.com>
2015-01-09 19:10:27 -0800

    new stuff

---
+++ GotCharge/GotCharge/DashBoardViewController.h
@@ -0,0 +1,13 @@
+//
+//  DashBoardViewController.h
+//  GotCharge
+//
+//  Created by Hemen Chhatbar on 1/9/15.
+//  Copyright (c) 2015 appforcause. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashBoardViewController : UIViewController
+
+@end
