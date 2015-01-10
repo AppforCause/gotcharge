1c11e0c77a680d9af32724bfa65cb31cbbc767fe
Hunaid <hunaid@hotmail.com>
2015-01-10 11:05:57 -0800

    Added login call in bmw client

--- GotCharge/BMWClient.m
+++ GotCharge/BMWClient.m
@@ -28,4 +28,20 @@
     return instance;
 }
 
+- (void)login {
+    // Request my request token
+    [self.requestSerializer removeAccessToken];
+    
+    [ self fetchRequestTokenWithPath:@"oauth/request_token" method:@"POST" callbackURL:[NSURL URLWithString:@"GotCharge://oauth"] scope:nil success:^(BDBOAuthToken *requestToken) {
+        NSLog(@"Got the request token");
+        // Now get the access code, this would take the user to a twitter mobile screen for login credentials
+        // and would return the control back to this application along with access code.
+        NSString *authURL = [NSString stringWithFormat:@"https://data.api.hackthedrive.com/v1/oauth/authorize?oauth_token=%@", requestToken.token];
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:authURL]];
+        
+    } failure:^(NSError *error) {
+        NSLog(@"failure to get the request token %@", [error description]);
+    }];
+}
+
 @end
