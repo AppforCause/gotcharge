4827f893a5971915afa9c32407960db2e21fbc74
Hemen Chhatbar <hemenc@slalom.com>
2015-01-11 09:29:20 -0800

    charge station view controller

---
+++ GotCharge/GotCharge/ChargeStationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ChargeStationViewController.h
+//  GotCharge
+//
+//  Created by Hemen Chhatbar on 1/11/15.
+//  Copyright (c) 2015 appforcause. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ChargeStationViewController : UIViewController
+
+@end
